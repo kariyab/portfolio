73bbf4235caa3a4d35394280c25676ad63ff46c0
kariya <sgkuarut@yahoo.co.jp>
2019-11-20 18:06:29 +0900

    user_index_01

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -53,9 +53,8 @@
 
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('messages.Logout') }}
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">{{ __('messages.Logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
